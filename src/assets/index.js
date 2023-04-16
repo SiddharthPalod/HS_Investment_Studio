3093a532250b8627a74d0724f57d37856028e8c2
SiddharthPalod <75942243+SiddharthPalod@users.noreply.github.com>
2023-04-16 21:26:40 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,94 @@
+import logo from "./logo.svg";
+import Venture from "./Venture.png";
+import Banker from "./Banker.png";
+import P2P from "./P2P.png";
+import angel from "./angel.png";
+import website from "./website.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import AXIS from "./tech/AXIS.png";
+import Canara from "./tech/Canara.png";
+import DCB from "./tech/DCB.png";
+import PnB from "./tech/PnB.png";
+import IDFC from "./tech/IDFC.png";
+import ICICI from "./tech/ICICI.png";
+import IndusInd from "./tech/IndusInd.png";
+import YES_BANK from "./tech/YES_BANK.png";
+import JP_Morgan from "./tech/JP_Morgan.png";
+import HSBC from "./tech/HSBC.png";
+import HDFC from "./tech/HDFC.png";
+import SBI from "./tech/SBI.png";
+import BOB from "./tech/BOB.svg";
+
+import M1 from "./company/M1.png";
+import Olymp_Trade from "./company/Olymp_Trade.png";
+import Quicken from "./company/Quicken.png";
+import StockMarketEye from "./company/StockMarketEye.png";
+
+import Mutual_Fund from "./Mutual_Fund.png";
+import Morgan_Stanley from "./Morgan_Stanley.png";
+import crypto from "./crypto.png";
+
+import a11 from "./about/a11.png";
+import a12 from "./about/a12.png";
+import a13 from "./about/a13.png";
+import a14 from "./about/a14.png";
+import a15 from "./about/a15.png";
+import a16 from "./about/a16.png";
+
+import a21 from "./about/a21.png";
+import a22 from "./about/a22.png";
+import a23 from "./about/a23.png";
+import a24 from "./about/a24.png";
+import a25 from "./about/a25.png";
+import a26 from "./about/a26.png";
+
+import a31 from "./about/a31.jpg";
+import a32 from "./about/a32.jpg";
+import a33 from "./about/a33.jpg";
+import a34 from "./about/a34.jpg";
+import a35 from "./about/a35.jpg";
+import a36 from "./about/a36.jpg";
+
+import a41 from "./about/a41.jpg";
+import a42 from "./about/a42.jpg";
+import a43 from "./about/a43.jpg";
+import a44 from "./about/a44.jpg";
+import a45 from "./about/a45.jpg";
+import a46 from "./about/a46.jpg";
+
+export {
+  logo,
+  Venture,
+  Banker,
+  P2P,
+  angel,
+  website,
+  menu,
+  close,
+  AXIS,
+  Canara,
+  DCB,
+  PnB,
+  IDFC,
+  ICICI,
+  IndusInd,
+  YES_BANK,
+  JP_Morgan,
+  HSBC,
+  HDFC,
+  SBI,
+  BOB,
+  M1,
+  Olymp_Trade,
+  Quicken,
+  StockMarketEye,
+  Mutual_Fund,
+  Morgan_Stanley,
+  crypto,
+  a11,a12,a13,a14,a15,a16,
+  a21,a22,a23,a24,a25,a26,
+  a31,a32,a33,a34,a35,a36,
+  a41,a42,a43,a44,a45,a46,
+};
